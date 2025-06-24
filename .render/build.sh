76a301841b52074143d01add08e8af9bcaef4939
tsvishnunarayanan <tsvishnu2005@gmail.com>
2025-06-25 00:51:23 +0530

    Update build.sh

--- .render/build.sh
+++ .render/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install -y wget gnupg
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
-apt-get update
-apt-get install -y google-chrome-stable
+
+set -x  # Show commands as they run
+
+# Install Chrome (headless)
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt update && apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Install Python packages
 pip install -r requirements.txt
