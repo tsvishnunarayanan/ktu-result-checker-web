81a3d46a227901da1d1fbac5650f1d79a15c242b
tsvishnunarayanan <tsvishnu2005@gmail.com>
2025-06-25 20:17:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def index():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 1000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
