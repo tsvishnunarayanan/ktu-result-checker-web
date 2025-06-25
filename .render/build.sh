a67fd3ffcc7b1b547dbaa22ddb60dfc397fd4495
tsvishnunarayanan <tsvishnu2005@gmail.com>
2025-06-25 12:40:15 +0530

    Update build.sh

--- .render/build.sh
+++ .render/build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# Install dependencies
-apt-get update
-apt-get install -y wget gnupg2
+# Install necessary tools
+apt-get update && apt-get install -y wget gnupg2
 
-# Download and install stable Google Chrome
+# Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb || apt install -f -y
 
-# Save path to chrome for later use
+# Save Chrome binary path
 which google-chrome > /opt/render/project/src/.chrome-bin
 
 # Install Python dependencies
